cedba464446acd8f52a2acc8818d6b3e17ba9d2c
AnnaMariaIsaev <114833688+AnnaMariaIsaev@users.noreply.github.com>
2022-12-03 16:46:12 -0800

    Update Intake.java

--- 2022-IAP-Intake/src/main/java/frc/robot/subsystems/Intake.java
+++ 2022-IAP-Intake/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,111 +4,51 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
-import com.ctre.phoenix.motorcontrol.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Intake extends SubsystemBase 
 {
-  private boolean override = false; // Helps us switch from manual to auto
-  private Timer overrideTimer = new Timer(); // We want a toggle button of some sorts
-  private double overrideTime = 1.0;
-
-  private final WPI_TalonSRX intakeFlyWheelTalon;
-
+  private final WPI_TalonSRX rightFlyWheelTalon;
+  private final WPI_TalonSRX leftFlyWheelTalon;
 
   public Intake() 
   {
-    intakeFlywheelTalon = new WPI_TalonSRX(Constants.IntakePorts.LeftFlywheelPort);
-    intakeFlyWheelTalon.setInverted(true); 
+    rightFlyWheelTalon = new WPI_TalonSRX(Constants.IntakePorts.rightFlyhweel);
+    leftFlyWheelTalon = new WPI_TalonSRX(Constants.IntakePorts.leftFlywheel);
 
-    intakeFlyWheel.configFactoryDefault();
-    intakeFlyWheel.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+    rightFlyWheelTalon.setInverted(true); 
+    leftFlyWheelTalon.setInverted(false);
 
-    overrideTimer.start(); // Start timer
-    overrideTimer.reset(); // Reset timer
+    leftFlyWheelTalon.configFactoryDefault();
+    rightFlyWheelTalon.configFactoryDefault();
   }
 
   public void setFlywheelPower(double speed) 
   {
-    intakeFlyWheel.set(speed);
+    leftFlyWheelTalon.set(ControlMode.PercentOutput, speed);
+    rightFlyWheelTalon.set(ControlMode.PercentOutput, speed);
   }
   
-  public boolean flywheelWithinErrorMargin() 
+  public double getCurrent()
   {
-    return (intakeFlyWheelPID.atSetpoint());
+    return leftFlyWheelTalon.getStatorCurrent();
   }
-
-  public void setFlywheelConstantVelocity(double RPM) 
-  {
-    intakeFlyWheel.setVoltage((intakeFlyWheelFF.calculate(RPM/60.0)) + intakeFlyWheelPID.calculate(getLeftRPM(), RPM));
-  }
-
-  public double getLeftRPM() 
-  {
-    return ((intakeFlyWheel.getSelectedSensorVelocity() * 10)/4096.0)*60.0;
-  }
-      public double getRightRPM() {
-      return ((intakeFlyWheel.getSelectedSensorVelocity() * 10)/4096.0)*60.0;
-      }
-      public double getLeftFlywheelPower() {
-      return intakeFlyWheel.get();
-      }
-      public double getRightFlywheelPower() {
-      return intakeFlyWheel.get();
-      }
-      public double getCurrent(){
-        return intakeFlyWheel.getStatorCurrent();
-      }
-      public double getAverageRPM() {
-        return ((getLeftRPM()/2.0));
-        }
-        public double getFlywheelCurrent() {
-        return (intakeFlyWheel.getStatorCurrent()/2.0);
-        
-        }
        
-        public void resetFlywheelEncoders() {
-        intakeFlyWheel.setSelectedSensorPosition(0, 0, 10);
-        }
-        
-
   @Override
   public void periodic() 
   {
-    // This method will be called once per scheduler run
-      SmartDashboard.putNumber("Average RPM", getAverageRPM());
-      SmartDashboard.putNumber("Average Current", getFlywheelCurrent());
-      SmartDashboard.putNumber("Left Flywheel RPM", getLeftRPM());
-      SmartDashboard.putNumber("Left Flywheel Power", getLeftFlywheelPower());
-      SmartDashboard.putNumber("Right Flywheel RPM", getRightRPM());
-      SmartDashboard.putNumber("Right Flywheel Power", getRightFlywheelPower());
-      SmartDashboard.putNumber("Intake Flywheel Current", getCurrent());
 
-if (RobotContainer.getJoy().getRawButton(2) && overrideTimer.get() >=  overrideTime) {
-  override = !override;
-  overrideTimer.reset();
-  }
-  if (override) { // Auto code
-    if (RobotContainer.getJoy1().getRawButton(1)) {
-    setFlywheelConstantVelocity(1000.0); // Sets it to 1000 RPM
-    } else {
-    setFlywheelConstantVelocity(0.0);
-    setFlywheelPower(0.0);
-    }
-    } else if (!override) { // Default manual override
-    setFlywheelPower(-1.0*RobotContainer.getJoy1().getY());
-    }
-      
   }
 }
 
