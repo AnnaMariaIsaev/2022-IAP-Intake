0a560de4bff344f630a012acc9e6177a5fb4ae1c
AnnaMariaIsaev <114833688+AnnaMariaIsaev@users.noreply.github.com>
2022-12-03 16:46:42 -0800

    Update RobotContainer.java

--- 2022-IAP-Intake/src/main/java/frc/robot/RobotContainer.java
+++ 2022-IAP-Intake/src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.SpinFlywheel;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Intake;
@@ -22,52 +21,34 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+public class RobotContainer 
+{
+  private static Joystick joy;
+  private static Intake intake;
 
-  private final Joystick joy;
-  private final Intake intake;
-  private final SpinFlywheel spinForward;
-  private final SpinFlywheel spinBackward;
-  private final SpinFlywheel stopSpin;
+  private final SpinFlywheel spinFly;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
 
-  public RobotContainer() {
+  public RobotContainer()
+  {
     // Configure the button bindings
-    joy = new Joystick(0);
+    joy = new Joystick(Constants.USBOrder.Zero);
     intake = new Intake();
-    spinForward = new SpinFlywheel(intake, 1);
-    spinBackward = new SpinFlywheel(intake, -1);
-    stopSpin = new SpinFlywheel(intake, 0);
 
+    spinFly = new SpinFlywheel(intake, joy);
   }
 
-  private void configureButtonBindings() {
-    JoystickButton forwardButton = new JoystickButton(joy, 5);
-    JoystickButton backwardButton = new JoystickButton(joy, 6);
-
-    forwardButton.whenPressed(spinForward);
-    backwardButton.whenPressed(spinBackward);
-
-    forwardButton.whenReleased(stopSpin);
-    backwardButton.whenReleased(stopSpin);
-  }
-
-  public Joystick getJoy()
+  public static Joystick getJoy()
   {
     return joy;
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+  private void configureButtonBindings() 
+  {}
+
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return spinFly;
   }
 }
